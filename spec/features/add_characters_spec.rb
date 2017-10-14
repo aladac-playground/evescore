8117ebe315c887407594516634c92b7e75ff0c3b
Adam Ladachowski <ladachowski@cictr.com>
2017-10-14 20:58:10 +0200

    - semi working scenario

--- spec/features/add_characters_spec.rb
+++ spec/features/add_characters_spec.rb
@@ -3,7 +3,9 @@
 require 'rails_helper'
 
 RSpec.feature 'AddCharacters', type: :feature do
-  scenario 'Add an EVE online character should succeed' do
-    visit user_crest_omniauth_authorize_path
+  scenario 'Add an EVE online character should succeed', js: true, vcr: true do
+    page.driver.browser.js_errors = false
+    visit '/users/auth/crest'
+    # save_and_open_page_with_css
   end
 end

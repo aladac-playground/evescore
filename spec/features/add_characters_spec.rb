27341e1e2d7ac90e76bf0e661ee8279c37272e98
Adam Ladachowski <ladachowski@cictr.com>
2017-10-14 12:33:41 +0200

    - rudimentary specs, not working

---
+++ spec/features/add_characters_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.feature "AddCharacters", type: :feature do
+  scenario "Add an EVE online character should succeed" do
+    visit user_crest_omniauth_authorize_path
+  end
+end

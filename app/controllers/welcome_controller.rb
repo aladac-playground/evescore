a0eb86629fe371d4095ae11fb044e9f5e37d693e
Adam Ladachowski <ladachowski@cictr.com>
2017-10-14 12:31:01 +0200

    - character list, character profile

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    @characters = current_user.characters || []
+  end
+  
+  def character_profile
+    @character = Character.find(params[:character_id])
+  end
 end

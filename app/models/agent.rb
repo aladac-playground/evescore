fa4b86e071d4ad66c086fe3755b6c57ef85dfbba
Adam Ladachowski <ladachowski@cictr.com>
2017-10-16 18:36:29 +0200

    - adding Agent model

---
+++ app/models/agent.rb
@@ -0,0 +1,8 @@
+class Agent
+  include Mongoid::Document
+  field :name, type: String
+  field :division, type: String
+  field :level, type: Integer
+  belongs_to :corporation
+  has_many :wallet_records
+end

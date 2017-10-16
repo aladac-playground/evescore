c37362fd2917150d96fc5a5aca79e025521a515b
Adam Ladachowski <ladachowski@cictr.com>
2017-10-16 18:37:33 +0200

    - adding Kill model

---
+++ app/models/kill.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class Kill
+  include Mongoid::Document
+  field :ts, type: Time
+  field :date, type: Date
+  field :amount, type: Integer
+  field :bounty, type: Float
+  belongs_to :wallet_record
+  belongs_to :rat, optional: true
+  after_create :create_rat
+  belongs_to :character
+
+  def create_rat
+    rat = Rat.where(id: rat_id).first_or_create
+    self.update_attributes(bounty: rat.bounty) if self.bounty.nil?
+  end
+  
+  def self.kills_by_bounty(character_id)
+    self.collection.aggregate([
+                                      { '$match' => { 'character_id' => character_id } },
+                                      { '$group' => {
+                                        '_id' => { rat_id: '$rat_id', bounty: '$bounty' },
+                                        'kills' => { '$sum' => '$amount' }
+                                      } },
+                                      { '$sort' => { '_id.bounty' => -1 } }
+                                    ])
+  end
+end

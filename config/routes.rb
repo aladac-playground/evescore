958b1b93477f750c037154d4371689fc6b040c9e
Adam Ladachowski <ladachowski@cictr.com>
2017-10-16 21:17:32 +0200

    - added destroy path, added name for profile path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@
 
 Rails.application.routes.draw do
   get 'welcome/index'
-  get 'character/:character_id', to: 'welcome#character_profile'
+  get 'character/:character_id', to: 'welcome#character_profile', as: :character_profile
+  delete 'character/:character_id/destroy', to: 'welcome#destroy', as: :character_destroy
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'welcome#index'

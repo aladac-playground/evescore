e8b6dba1f23d8ef33c809f508a55782ef1dec1fb
Adam Ladachowski <ladachowski@cictr.com>
2017-10-16 18:33:57 +0200

    - getting data to display in profile

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,10 @@ class WelcomeController < ApplicationController
   end
 
   def character_profile
-    @character = Character.find(params[:character_id])
+    @character = Character.find(params[:character_id].to_i)
+    @ticks = @character.wallet_records.order('ts desc').limit(10)
+    @bounty_by_day = @character.bounty_by_day[0..9]
+    @valuable_rats = @character.kills.order("bounty desc").limit(10)
+    @top_ticks = @character.wallet_records.order('amount desc').limit(10)
   end
 end

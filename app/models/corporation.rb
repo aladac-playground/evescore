7738f80002148364b6895a70d79f92d66500007c
Adam Ladachowski <ladachowski@cictr.com>
2017-10-16 21:14:41 +0200

    - create corp from api

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -4,6 +4,12 @@ class Corporation
   include Mongoid::Document
   field :name, type: String
   field :npc, type: Boolean
+  field :ticker, type: String
   has_many :characters
   has_many :agents
+
+  def self.create_from_api(corporation_id)
+    api_corporation = ESI::CorporationApi.new.get_corporations_corporation_id(corporation_id)
+    where(id: corporation_id, name: api_corporation.corporation_name, ticker: api_corporation.ticker, npc: false).first_or_create
+  end
 end

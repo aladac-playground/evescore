801bcc4b02fc24f483cf5b84899429b1ab5e8034
Adam Ladachowski <ladachowski@cictr.com>
2017-10-16 21:17:03 +0200

    - gmail production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,12 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com', port: 587, user_name: ENV['gmail_username'],
+    password: ENV['gmail_password'], authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter

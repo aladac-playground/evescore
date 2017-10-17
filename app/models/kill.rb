712f87033c2b2612ec4dec9c9be0284f1f9dce21
Adam Ladachowski <ladachowski@cictr.com>
2017-10-17 12:42:12 +0200

    added factions and a faction based aggregation

--- app/models/kill.rb
+++ app/models/kill.rb
@@ -8,12 +8,14 @@ class Kill
   field :bounty, type: Float
   belongs_to :wallet_record
   belongs_to :rat, optional: true
+  belongs_to :faction, optional: true
   after_create :create_rat
   belongs_to :character
 
   def create_rat
     rat = Rat.where(id: rat_id).first_or_create
     update_attributes(bounty: rat.bounty) if bounty.nil?
+    update_attributes(faction_id: rat.faction_id) if faction_id.nil?
   end
 
   def self.kills_by_bounty(character_id)
@@ -24,6 +26,17 @@ class Kill
                              'amount' => { '$sum' => '$amount' }
                            } },
                            { '$sort' => { '_id.bounty' => -1 } }
+                         ])
+  end
+
+  def self.kills_by_faction(character_id)
+    collection.aggregate([
+                           { '$match' => { 'character_id' => character_id } },
+                           { '$group' => {
+                             '_id' => '$faction_id',
+                             'amount' => { '$sum' => '$amount' }
+                           } },
+                           { '$sort' => { 'amount' => -1 } }
                          ])
   end
 end

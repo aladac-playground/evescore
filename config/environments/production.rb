a09d67f29ffcfa13de3f4a84230b7aaa434dbcb9
Adam Ladachowski <ladachowski@cictr.com>
2017-10-17 16:45:51 +0200

    stfu rubocop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com', port: 587, user_name: ENV['gmail_username'],
-    password: ENV['gmail_password'], authentication: 'plain',
+    address: 'smtp.gmail.com', port: 587, user_name: ENV['gmail_username'], password: ENV['gmail_password'], authentication: 'plain',
     enable_starttls_auto: true
   }
 

339c29962f09b0650493c4a6abd8fa85664856c3
Adam Ladachowski <ladachowski@cictr.com>
2017-10-17 12:42:57 +0200

    Faction model

---
+++ app/models/faction.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Faction
+  include Mongoid::Document
+  field :name, type: String
+  field :pattern, type: Regexp
+  validates :name, uniqueness: true
+  belongs_to :corporation
+  has_many :rats
+
+  def self.detect(string)
+    output = nil
+    all.each do |faction|
+      next if faction.pattern.nil?
+      if faction.pattern.compile.match?(string)
+        output = faction
+        break
+      end
+    end
+    output
+  end
+end

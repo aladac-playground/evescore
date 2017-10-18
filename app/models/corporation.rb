a8c3a3a811dd906909e4e6d0510ec4afdfd70a0b
Adam Ladachowski <ladachowski@cictr.com>
2017-10-18 11:53:53 +0200

    - do not pass npc argument when creating a company for user

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -7,9 +7,9 @@ class Corporation
   field :ticker, type: String
   has_many :characters
   has_many :agents
-
+  
   def self.create_from_api(corporation_id)
     api_corporation = ESI::CorporationApi.new.get_corporations_corporation_id(corporation_id)
-    where(id: corporation_id, name: api_corporation.corporation_name, ticker: api_corporation.ticker, npc: false).first_or_create
+    where(id: corporation_id, name: api_corporation.corporation_name, ticker: api_corporation.ticker).first_or_create
   end
 end

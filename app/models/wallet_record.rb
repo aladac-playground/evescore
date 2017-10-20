aee7578c8638ae831ffba249e5918a519f3a7eb4
Adam Ladachowski <ladachowski@cictr.com>
2017-10-20 20:49:25 +0200

    - added mission levels - added time rewards to importable types - guard clause to ignore non bounty text to parse rats

--- app/models/wallet_record.rb
+++ app/models/wallet_record.rb
@@ -8,14 +8,16 @@ class WalletRecord
   field :tax, type: Float
   field :type, type: String
   field :ref_id, type: Integer
+  field :mission_level, type: Integer
   index({ character_id: 1, ts: 1 }, unique: true, drop_dups: true)
   belongs_to :character
   belongs_to :user, optional: true
   belongs_to :agent, optional: true
   has_many :kills, autosave: true
-  validates :ts, uniqueness: { scope: :character_id }
+  validates :ts, uniqueness: { scope: %i[character_id ref_id] }
 
   IMPORTABLE_REF_TYPES = %w[
+    agent_mission_time_bonus_reward
     agent_mission_reward
     bounty_prizes
   ].freeze
@@ -27,10 +29,17 @@ class WalletRecord
   def self.create_from_api(character_id, user_id, wallet_record)
     record = new(
       amount: wallet_record.amount, tax: wallet_record.tax || 0, ts: wallet_record.date, date: wallet_record.date.to_date, user_id: user_id,
-      type: wallet_record.ref_type, ref_id: wallet_record.ref_id, character_id: character_id, agent_id: wallet_record.first_party_id
+      type: wallet_record.ref_type, ref_id: wallet_record.ref_id, character_id: character_id, agent_id: wallet_record.first_party_id,
+      mission_level: mission_level(wallet_record.first_party_id)
     )
     record.build_kills(wallet_record.reason)
     record.save
+  end
+
+  def self.mission_level(agent_id)
+    Agent.find(agent_id).level
+  rescue Mongoid::Errors::DocumentNotFound
+    nil
   end
 
   def parse_rats(text)
@@ -38,6 +47,7 @@ class WalletRecord
   end
 
   def build_kills(text)
+    return false if text.blank? || text.match(/(\d+:+\s+\d+,?)+/).blank?
     parse_rats(text).each do |rat_info|
       kills.build(rat_id: rat_info[0].to_i, amount: rat_info[1], date: date, ts: ts, character_id: character_id, user_id: user_id)
     end

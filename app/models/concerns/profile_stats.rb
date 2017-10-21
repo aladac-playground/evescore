bb6b743da791dea838ee488926dfbb2ac54fd842
Adam Ladachowski <ladachowski@cictr.com>
2017-10-21 12:46:09 +0200

    - ded sites and missions

--- app/models/concerns/profile_stats.rb
+++ app/models/concerns/profile_stats.rb
@@ -12,6 +12,28 @@ module ProfileStats
     WalletRecord.collection.aggregate pipeline
   end
 
+  def missions_by_level(limit = nil)
+    query = wallet_records.missions.group(_id: '$mission_level', :amount.sum => 1)
+                          .project(_id: 0, level: '$_id', amount: 1)
+                          .desc(:amount)
+    pipeline = query.pipeline
+    pipeline.push('$limit' => limit) if limit
+    WalletRecord.collection.aggregate pipeline
+  end
+
+  def ded_sites_by_site(limit = nil)
+    query = wallet_records.ded_sites.group(_id: '$ded_site_id', :amount.sum => 1)
+    pipeline = query.pipeline
+    pipeline += [
+      { '$lookup' => { from: 'ded_sites', localField: '_id', foreignField: '_id', as: 'ded_site' } },
+      { '$project' => { _id: 1, amount: 1, ded_site: { '$arrayElemAt' => ['$ded_site', 0] } } },
+      { '$project' => { _id: 0, amount: 1, name: '$ded_site.name', level: '$ded_site.level', ded_faction_id: '$ded_site.faction_id' } },
+      { '$sort' => { 'amount' => -1 } }
+    ]
+    pipeline.push('$limit' => limit) if limit
+    WalletRecord.collection.aggregate pipeline
+  end
+
   def kills_by_bounty(limit = nil)
     query = kills.group(_id: { rat_id: '$rat_id', bounty: '$bounty' }, :amount.sum => '$amount')
     pipeline = query.pipeline
@@ -54,5 +76,21 @@ module ProfileStats
 
   def favourite_faction
     kills_by_faction.first || OpenStruct.new
+  end
+
+  def favourite_ded_site
+    ded_sites_by_site.first
+  end
+
+  def favourite_mission_level
+    missions_by_level.first.try(:[], 'level') || '-'
+  end
+
+  def ded_sites_run
+    wallet_records.where(:ded_site.ne => nil).count
+  end
+
+  def missions_run
+    wallet_records.where(:mission_level.ne => nil).count
   end
 end

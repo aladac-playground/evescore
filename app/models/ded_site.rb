9451510e68cf32c1719254da1540e3c2baf7633c
Adam Ladachowski <ladachowski@cictr.com>
2017-10-21 12:46:24 +0200

    - added faction relation

--- app/models/ded_site.rb
+++ app/models/ded_site.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 class DedSite
   include Mongoid::Document
   field :name, type: String
   field :level, type: String
   field :boss_id, type: Integer
+  belongs_to :faction
 end

57b1439d2ba9226d198494007bc6c5b22617cb04
Adam Ladachowski <ladachowski@cictr.com>
2017-10-27 15:20:31 +0200

    - moved image helpers to a separate file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,53 +25,12 @@ module ApplicationHelper
     link_to image_tag('EVE_SSO_Login_Buttons_Small_Black.png'), user_crest_omniauth_authorize_path
   end
 
-  def navbar_portrait(character)
-    title = character.name
-    character_image(character.id, 32, tooltip(title))
-  end
-
-  def character_image(character_id, size = 64, options = {})
-    options[:class] = 'img-rounded portrait'
-    image_tag "https://image.eveonline.com/Character/#{character_id}_#{size}.jpg", options
-  end
-
-  def type_image(type_id, size = 32, options = {})
-    ded_boss = DedSite.all.map(&:boss_id).include?(type_id)
-    options[:class] = 'img-rounded cursor-hand'
-    options[:style] = 'border: 1px solid; border-color: red' if ded_boss
-    # concat = '&nbsp;'.html_safe
-    image_tag("https://image.eveonline.com/Type/#{type_id}_#{size}.png", options)
-  end
-
-  def kill_image(kill)
-    kill = OpenStruct.new(kill) if kill.class == BSON::Document
-    title = "<strong>#{kill.name}</strong><br>#{kill.amount} kills"
-    type_image(kill.rat_id, 32, tooltip(title))
-  rescue ActionView::Template::Error
-    ''
-  end
-
   def number_to_isk(amount)
     number_to_currency amount, format: '%n %u', precision: 0, unit: 'ISK'
   end
 
   def number_to_isk_short(amount)
     number_to_human amount, units: { thousand: 'K ISK', million: 'M ISK', billion: 'b ISK' }, format: '%n%u'
-  end
-
-  def isk_image
-    type_image(29, 32)
-  end
-
-  def corporation_image(corporation_id, size = 32, options = {})
-    options[:class] = 'img-rounded'
-    image_tag("https://image.eveonline.com/Corporation/#{corporation_id}_#{size}.png", options)
-  end
-
-  def faction_image(faction_id)
-    return '-' if faction_id.blank?
-    faction = Faction.find(faction_id)
-    corporation_image faction.corporation_id, 32, tooltip(faction.corporation.name)
   end
 
   def record_icon(record)

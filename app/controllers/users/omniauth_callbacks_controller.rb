500b50b06101031244f4e5a44cb7b9c52d8cd434
Adam Ladachowski <ladachowski@cictr.com>
2017-10-27 16:47:27 +0200

    - cosmetic changes

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,6 +5,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     redirect_to root_path && return unless current_user
     omniauth_payload = request.env['omniauth.auth']
     current_user.import_character(omniauth_payload)
-    redirect_to root_path, notice: 'Your character has been successfully imported. Initial sync has been queued. In a moment your data will show up'
+    redirect_to characters_path, notice: 'Your character has been successfully imported. Initial sync has been queued. In a moment your data will show up'
   end
 end

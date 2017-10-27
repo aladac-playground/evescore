2f6a9046d73884eb1418bda9c78afa35aaa6c4a0
Adam Ladachowski <ladachowski@cictr.com>
2017-10-27 20:32:38 +0200

    - cleanup

--- app/models/kill.rb
+++ app/models/kill.rb
@@ -20,37 +20,4 @@ class Kill
     update_attributes(bounty: rat.bounty) if bounty.nil?
     update_attributes(faction_id: rat.faction_id) if faction_id.nil?
   end
-
-  def self.user_kills_by_bounty(user_id)
-    collection.aggregate([
-                           { '$match' => { 'user_id' => user_id } },
-                           { '$group' => {
-                             '_id' => { rat_id: '$rat_id', bounty: '$bounty' },
-                             'amount' => { '$sum' => '$amount' }
-                           } },
-                           { '$sort' => { '_id.bounty' => -1 } }
-                         ])
-  end
-
-  def self.kills_by_faction(character_id)
-    collection.aggregate([
-                           { '$match' => { 'character_id' => character_id } },
-                           { '$group' => {
-                             '_id' => '$faction_id',
-                             'amount' => { '$sum' => '$amount' }
-                           } },
-                           { '$sort' => { 'amount' => -1 } }
-                         ])
-  end
-
-  def self.user_kills_by_faction(user_id)
-    collection.aggregate([
-                           { '$match' => { 'user_id' => user_id } },
-                           { '$group' => {
-                             '_id' => '$faction_id',
-                             'amount' => { '$sum' => '$amount' }
-                           } },
-                           { '$sort' => { 'amount' => -1 } }
-                         ])
-  end
 end
